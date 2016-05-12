e501b790899a9732facdf129e8a9b19d2d53498c
rajeev021 <rajeev.p@ivoice.in>
2016-05-12 14:53:57 +0530

    Initial Commit

---
+++ sampleRepo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleRepo
+//
+//  Created by DODO on 12/05/16.
+//  Copyright © 2016 DODO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleRepo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleRepo
+//
+//  Created by DODO on 12/05/16.
+//  Copyright © 2016 DODO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
